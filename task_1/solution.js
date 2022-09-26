564a75d9675a90e4021aef59d8f6d8d531d1bfd8
Leonid Osipov <osik174@yandex.ru>
2022-09-26 23:45:51 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -56,7 +56,8 @@ function addInCartSumChange(sum, delta) {
 let newSum;
 newSum = oldSum + difference;
 let newSumText;
-newSumText = newSum + "₽";
+newSumText = newSum + " ₽ ";
+newSumText = `${newSum} ₽`;
 
 console.log (newSumText);
 
