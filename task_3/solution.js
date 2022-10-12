70c667c34a5a218b564800efe39b858a7967e6d7
Leonid Osipov <osik174@yandex.ru>
2022-10-12 16:11:14 +0300

    task 3

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,15 +3,15 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
     
-    let itemCountText = "";
+    let itemCountText = "" ;
     itemCountText = itemCountText + product.count;
-    itemCountText = " × ";
+    itemCountText = " × " ;
     itemCountText = itemCountText + product.price;
     itemCountText = itemCountText + " ₽ = ";
     let sum = product.count * product.price;
     itemCountText = itemCountText + sum;
-    itemCountText = itemCountText + " ₽ ";
-
+    itemCountText = itemCountText + " ₽ "; 
+ 
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
